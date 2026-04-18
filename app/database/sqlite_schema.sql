5506295cef0624a343dcc9efffbd2dc7522a14e0
volodymyrdmytrenko <volodymyr.dmytrenko@gmail.com>
2026-04-18 22:15:05 +0300

    sqlite schema bug fix

--- app/database/sqlite_schema.sql
+++ app/database/sqlite_schema.sql
@@ -21,12 +21,11 @@ CREATE TABLE IF NOT EXISTS visits (
     FOREIGN KEY (employee_id) REFERENCES employees(id)
 );
 
-CREATE UNIQUE INDEX ux_visits_employee_day
-ON visits(employee_id, date(visit_time));
-CREATE UNIQUE INDEX ux_visits_sync_uuid ON visits(sync_uuid);
-CREATE INDEX ix_visits_visit_time ON visits(visit_time);
-CREATE INDEX ix_visits_synced ON visits(synced);
-CREATE INDEX ix_visits_employee_visit_time ON visits(employee_id, visit_time);
+CREATE UNIQUE INDEX IF NOT EXISTS ux_visits_employee_day ON visits(employee_id, date(visit_time));
+CREATE UNIQUE INDEX IF NOT EXISTS ux_visits_sync_uuid ON visits(sync_uuid);
+CREATE INDEX IF NOT EXISTS ix_visits_visit_time ON visits(visit_time);
+CREATE INDEX IF NOT EXISTS ix_visits_synced ON visits(synced);
+CREATE INDEX IF NOT EXISTS ix_visits_employee_visit_time ON visits(employee_id, visit_time);
 
 CREATE TABLE IF NOT EXISTS sessions (
     session_id TEXT PRIMARY KEY,
